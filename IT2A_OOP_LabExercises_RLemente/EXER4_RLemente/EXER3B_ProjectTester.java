2b234ac6d00c2f2d78c70f363dcf3d0ea6ddace0
Lemente, Rechel Mae L. <lementerechelmae@gmail.com>
2025-09-25 18:43:42 +0800

    Update EXER3B_ProjectTester.java

--- IT2A_OOP_LabExercises_RLemente/EXER4_RLemente/EXER3B_ProjectTester.java
+++ IT2A_OOP_LabExercises_RLemente/EXER4_RLemente/EXER3B_ProjectTester.java
@@ -1,9 +1,1 @@
-public class EXER3B_ProjectTester {
-    public static void main(String[] args) {
-        EXER3B_User user1 = new EXER3B_User("Alice");
-        EXER3B_User user2 = new EXER3B_User("Bob");
 
-        user1.displayInfo();
-        user2.displayInfo();
-    }
-}
