0a88d7cee17d9b0fcfb0f7dfc4009826d561494b
Lemente, Rechel Mae L. <lementerechelmae@gmail.com>
2025-09-25 18:44:10 +0800

    Update EXER4_Admin.java

--- IT2A_OOP_LabExercises_RLemente/EXER4_RLemente/EXER4_Admin.java
+++ IT2A_OOP_LabExercises_RLemente/EXER4_RLemente/EXER4_Admin.java
@@ -1,11 +1,1 @@
-public class EXER4_Admin extends EXER4_User {
-    public EXER4_Admin(String name) {
-        super(name);
-        this.role = "Administrator";
-    }
 
-    @Override
-    public void displayInfo() {
-        System.out.println("Admin User: " + name + " | Role: " + role + " | Has full access.");
-    }
-}
