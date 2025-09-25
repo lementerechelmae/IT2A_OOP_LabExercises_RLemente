6830cc88cc2476a0b2611165ed6710c164d68d38
Lemente, Rechel Mae L. <lementerechelmae@gmail.com>
2025-09-25 18:43:54 +0800

    Update EXER3B_User.java

--- IT2A_OOP_LabExercises_RLemente/EXER4_RLemente/EXER3B_User.java
+++ IT2A_OOP_LabExercises_RLemente/EXER4_RLemente/EXER3B_User.java
@@ -1,13 +1,1 @@
-public class EXER3B_User {
-    protected String name;
-    protected String role;
 
-    public EXER3B_User(String name) {
-        this.name = name;
-        this.role = "Generic User";
-    }
-
-    public void displayInfo() {
-        System.out.println("User: " + name + " | Role: " + role);
-    }
-}
